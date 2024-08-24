a45db63688c01bc8ab4e79d5940c37076ec9db50
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-08-24 15:57:30 +0200

    getpost
    getposts funkcija, fetcha sve objave sa baze,

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { verifyToken } from "../Handlers/checkUserAuth";
-import { create } from "../Handlers/post.handler";
+import { create, getposts } from "../Handlers/post.handler";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-
+router.get("/getposts", getposts);
 export default router;
