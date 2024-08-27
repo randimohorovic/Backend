30aaa1194c9856085ec706ed7e8ff3e552eb2883
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-08-27 12:42:38 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import authRouter from "../routes/auth.js";
 import postRouter from "../routes/post.route.js";
 import ratingRouter from "../routes/rating.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 mongoose.connect(
   "mongodb+srv://admin:adminjobquest@jobquest.xzerq1j.mongodb.net/?retryWrites=true&w=majority&appName=jobquest"
 );
+const deploy = path.resolve();
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -37,6 +39,14 @@ app.use("/backend/user", userRouter);
 app.use("/backend/auth", authRouter);
 app.use("/backend/post", postRouter);
 app.use("/backend/rating", ratingRouter);
+
+// Serve static files from the React app
+app.use(express.static(path.join(deploy, "../Frontend/Jobquest/build")));
+
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(deploy, "../Frontend/Jobquest/build", "index.html"));
+});
 //error koj dolazi iz inputa, req data iz brosera, response je odg servera na kljentu stranu
 //error
 app.use((error, req, res, next) => {
